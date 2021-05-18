7355a940f559dc5c190ca5af3b0a4fe277e09045
maximilian.koschay <mkoschay@web.de>
2021-05-18 13:50:30 +0200

    Change of plan breakpoint

--- mflowgen/flows/Commercial_Synthesis/innovus-customizations/outputs/setup.tcl
+++
@@ -1,8 +0,0 @@
-# Sepcify the initial Floorplan DEF file
-set vars(def_files)                  inputs/user_project_wrapper.def
-
-# Correct the names of the power nets:
-set vars(power_nets)  "vdda1 vdda2 vccd1 vccd2"
-set vars(ground_nets) "vssa1 vssa2 vssd1 vssd2"
-
-
